409dc307c4c7fa64eec5a7eee4ec26d28c0c5119
zzdpk2 <zzdpk2@126.com>
2020-05-07 02:01:06 -0500

    new

---
+++ awesomeProject/src/test/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Hi!")
+	fmt.Print("New Program")
+
+	var dummyHead int = 123
+	sum := dummyHead + 6
+	fmt.Print("Sum:", sum)
+	fmt.Print("dummyHead:", dummyHead)
+
+}
---
+++ test.go
@@ -0,0 +1,12 @@
+package practice
+
+import "fmt"
+
+func add (a int, b int) int {
+	return a + b
+}
+
+func main() {
+
+	fmt.Print()
+}
