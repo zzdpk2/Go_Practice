ec63f9366d5be10489dd524e15eec3509a19296d
Rog_ubuntu_v_ <zzdpk2@126.com>
2020-05-08 12:08:27 -0700

    new files

--- awesomeProject/src/03Keywords.go
+++ awesomeProject/src/03Keywords.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"reflect"
 )
 
 func main01() {
@@ -67,15 +68,68 @@ func main01() {
 	f2 := func(a int, b int) {
 		fmt.Println(a + b)
 	}
-
 	fmt.Printf("%T\n", f2)
-
 }
 
-func main() {
+// self define function type
+type FUNCTYPE func(int, bool, string)
+
+func demo(a int, b bool, c string) {
+	fmt.Println("new function was created!")
+}
+
+// function recall
+func demo2(f FUNCTYPE) {
+	f(1, true, "Good")
+}
+
+func main02() {
 	// type
 	// 1. create alias for existed type
 	main01()
-	type i8 int8
+	type i8 = int8
+	//var a i8 = 123
+	//var b i8 = 2
+	var b i8 = 2
+	var a int8 = 1
+	fmt.Println(a + b)
+
+	var f FUNCTYPE
+	f = demo
+	f(1, false, "good")
+
+	var f2 FUNCTYPE
+	f2 = demo
+	demo2(f2)
+}
+
+func main03() {
+	// reflection and interface
+	// Empty type of interface that can accept all types of params
+	var a interface{}
+	a = 1
+	a = 12.3
+	a = true
+	a = 1
+	// Reflection operation to get type of a interface
+	t := reflect.TypeOf(a)
+	fmt.Println(t)
+	// Reflection operation to get value of a interface
+	// The type os v is not int instead it is reflect.value
+	a1, ok := a.(int)
+	fmt.Println(a1)
+	fmt.Println(ok)
+	fmt.Printf("a1 type: %T\n", a1)
+	fmt.Printf("ok type: %T\n", ok)
+
+	v := reflect.ValueOf(a)
+	fmt.Println(v)
+	fmt.Println(v.Kind())
+	fmt.Println(v.Int())
+	a = "Niu bi!"
+	fmt.Println(a)
+}
+
+func main() {
 
 }
