db44a2a95577a85c969b2b617262185f37adba12
zzdpk2 <zzdpk2@126.com>
2020-11-24 08:34:29 +0800

    "Update Golang Learning files"

---
+++ learngo/src/func.go
@@ -0,0 +1,19 @@
+package main
+
+func eval(a, b int, op string) int {
+	switch op {
+	case "+":
+		return a + b
+	case "-":
+		return a - b
+	case "*":
+		return a * b
+	case "/":
+		return a / b
+	default:
+		panic("unsupported operatio: " + op)
+	}
+}
+
+// Return two vals
+//func
--- learngo/src/loop.go
+++ learngo/src/loop.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func convertToBin(n int) interface{} {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
+
+func forever() {
+	i := 0
+	for {
+		fmt.Println("abc")
+		if i == 10 {
+			return
+		}
+		i++
+	}
+}
+
+func main() {
+	fmt.Println(
+		convertToBin(5),
+		convertToBin(13),
+		convertToBin(9999999),
+		convertToBin(0))
+	printFile("abc.txt")
+	forever()
+}
