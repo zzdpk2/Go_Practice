adf94f36c7a91f43797360546317de21972fd788
zzdpk2 <zzdpk2@126.com>
2021-11-16 12:09:43 +0800

    Add drafts

--- gop1/arrays.go
+++ gop1/arrays.go
@@ -6,7 +6,18 @@ func main() {
 	var arr1 [5]int
 	arr2 := [3]int{1, 3, 5}
 	arr3 := [...]int{2, 4, 6, 8, 10}
+	var grid [4][5]int
 
 	fmt.Println(arr1, arr2, arr3)
-
+	fmt.Println(grid)
 }
+
+// 统计方法与概率论
+// 概率分布
+// 线性代数
+// 假设检定
+// 统计建模与拟合
+// 数据摘要与描述性统计
+// 回归分析
+// 贝叶斯思维与建模
+// 马尔科夫链
