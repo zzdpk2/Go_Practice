a96e32df09c8fa52c517e72226023d3d34dded47
zzdpk2 <zzdpk2@126.com>
2021-11-16 14:53:48 +0800

    add arrays

--- gop1/arrays.go
+++ gop1/arrays.go
@@ -2,7 +2,21 @@ package main
 
 import "fmt"
 
+func printArray(arr [5]int) {
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+}
+
+func printArray2(arr *[5]int) {
+	for i, v := range arr {
+		fmt.Println(i, v)
+	}
+	arr[0] = 100
+}
+
 func main() {
+	// var arr1 [5]int
 	var arr1 [5]int
 	arr2 := [3]int{1, 3, 5}
 	arr3 := [...]int{2, 4, 6, 8, 10}
@@ -10,14 +24,27 @@ func main() {
 
 	fmt.Println(arr1, arr2, arr3)
 	fmt.Println(grid)
-}
 
-// 统计方法与概率论
-// 概率分布
-// 线性代数
-// 假设检定
-// 统计建模与拟合
-// 数据摘要与描述性统计
-// 回归分析
-// 贝叶斯思维与建模
-// 马尔科夫链
+	// for i := 0; i < len(arr3); i++ {
+	// 	fmt.Println(arr3[3])
+	// }
+
+	for i, v := range arr3 {
+		fmt.Println(i, v)
+	}
+
+	// Find out max value and its index
+	numbers := [...]int{3, 7, 1, 9, 10, 78, 34}
+	maxi, maxValue := -1, -1
+	for i, v := range numbers {
+		if v > maxValue {
+			maxi, maxValue = i, v
+		}
+	}
+	fmt.Println("maxi is : ", maxi)
+	fmt.Println("maxValue is :", maxValue)
+
+	printArray(arr3)
+	printArray2(&arr3)
+	fmt.Println(arr3)
+}
