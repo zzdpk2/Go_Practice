3fc7f04708206b34de693522fe43454dc14f4b96
zzdpk2 <zzdpk2@126.com>
2021-11-16 16:52:22 +0800

    adjust the structure and start learning map

---
+++ gop1/container/maps.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]string{
+		"name":    "aa",
+		"course":  "golang",
+		"site":    "zebra",
+		"quality": "notbad",
+	}
+
+	fmt.Println(m)
+}
