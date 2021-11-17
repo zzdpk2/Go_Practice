5e06cd6a34a798279a80d65aa4d43348fcb5d209
zzdpk2 <zzdpk2@126.com>
2021-11-17 13:25:36 +0800

    add map usage

--- gop1/container/maps.go
+++ gop1/container/maps.go
@@ -7,8 +7,22 @@ func main() {
 		"name":    "aa",
 		"course":  "golang",
 		"site":    "zebra",
-		"quality": "notbad",
+		"quality": "not bad",
 	}
 
-	fmt.Println(m)
+	m2 := make(map[string]int) // m2 is empty map
+
+	var m3 map[string]string // m3 is nil
+
+	fmt.Println(m, m2, m3)
+
+	fmt.Println("Traversing map:")
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	fmt.Println("Getting values:")
+	courseName := m["course"]
+	fmt.Println(courseName)
 }
