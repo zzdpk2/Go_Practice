b74a5ff4f71791d965ddf81cbba5d37f79ec1d0a
zzdpk2 <zzdpk2@126.com>
2021-12-05 01:19:50 +0800

    adding grammar for map

--- gop1/container/maps.go
+++ gop1/container/maps.go
@@ -25,4 +25,26 @@ func main() {
 	fmt.Println("Getting values:")
 	courseName := m["course"]
 	fmt.Println(courseName)
+
+	causeName := m["cause"]
+	fmt.Println("cause val", causeName)
+
+	if causeName1, ok := m["cause"]; ok {
+		fmt.Println("cause2 val: ", causeName1)
+	} else {
+		fmt.Println("key does not exist")
+	}
+
+	// Delete values
+	fmt.Println("Deleting values:")
+	name, ok := m["name"]
+	fmt.Println(name, ok)
+
+	// After deleting
+	delete(m, "name")
+	name, ok = m["name"]
+	fmt.Println("name:", name, ok)
+
+	// What elements cannot be keys? The elements cannot be evaluated such as slice, map, and functions.
+	// Struct without those data structures can be the key as well.
 }
