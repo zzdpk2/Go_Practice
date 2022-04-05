7775eaf01d7bdbfc293e6aeaba8c60e01848c10b
zzdpk2 <zzdpk2@126.com>
2022-04-05 23:43:25 +0800

    rune

--- goByExam/src/18-String-Rune/18-String-Rune.go
+++ goByExam/src/18-String-Rune/18-String-Rune.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	const s = "สวัสดี"
+	fmt.Println("Len of s: ", len(s))
+
+	for i := 0; i < len(s); i++ {
+		fmt.Printf("%x ", s[i])
+	}
+
+	fmt.Println()
+
+	fmt.Println("Rune count: ", utf8.RuneCountInString(s))
+
+	fmt.Println("\nUsing DecodeRuneInString")
+
+}
